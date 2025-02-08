857a39d9a0613e264d5dff0c43ba33dfd11e78f0
sahand mahdilu <sahand.mahdilu29@gmail.com>
2025-02-08 12:16:58 +0330

    feat : addTodo added

--- src/Components/TodoList.jsx
+++ src/Components/TodoList.jsx
@@ -5,6 +5,7 @@ import { useState } from "react";
 
 export default function TodoList() {
     const [title ,setTitle]=useState("")
+    const [todos, setTodos]= useState([])
 
 
     const todoTitleHandler=(event)=>{
@@ -14,6 +15,26 @@ export default function TodoList() {
       
     }
 
+    const addTodo= (event)=>{
+
+        event.preventDefault()
+
+        let newTodoObject={
+            id:todos.length+1,
+            todoTitle:title,
+            status:false
+        }
+
+        setTodos(prev=>{
+
+          return [...prev,newTodoObject]
+        })
+
+        
+        
+    }
+    
+    console.log(todos);
 
 
 
@@ -23,9 +44,9 @@ export default function TodoList() {
         <Header/>
 
         <div>
-            <form className="flex justify-center items-center gap-3 mt-5 " action="">
+            <form onSubmit={addTodo} className="flex justify-center items-center gap-3 mt-5 " action="">
                 <div className="w-[40%] relative ">
-                <input onChange={todoTitleHandler} className="p-2 pr-10 w-full " placeholder="Enter todo..." type="text"name="todo-name" />
+                <input onChange={todoTitleHandler} value={title} className="p-2 pr-10 w-full " placeholder="Enter todo..." type="text"name="todo-name" />
                 <button type="submit">
                 <Icon className=" hover:bg-blue-300 hover:text-white text-green-400  absolute right-0 top-1/2 transform -translate-y-1/2" icon="mynaui:plus-square" width="40" height="40" />
                 </button>
