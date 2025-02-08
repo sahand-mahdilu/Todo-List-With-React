405d2d5a06a2a27385f02fee380c12088d84414f
sahand mahdilu <sahand.mahdilu29@gmail.com>
2025-02-08 18:32:53 +0330

    feat : removeing todos added

--- src/Components/TodoList.jsx
+++ src/Components/TodoList.jsx
@@ -4,93 +4,86 @@ import Todos from "./Todos";
 import { useState } from "react";
 
 export default function TodoList() {
-    const [title ,setTitle]=useState("")
-    const [todos, setTodos]= useState([])
+  const [title, setTitle] = useState("");
+  const [todos, setTodos] = useState([]);
 
+  const todoTitleHandler = (event) => {
+    setTitle(event.target.value);
+  };
 
-    const todoTitleHandler=(event)=>{
+  const addTodo = (event) => {
+    event.preventDefault();
 
-        setTitle(event.target.value)
-
-      
+    if (title) {
+      let newTodoObject = {
+        id: todos.length + 1,
+        todoTitle: title,
+        status: false,
+      };
+      setTodos((prev) => {
+        return [...prev, newTodoObject];
+      });
     }
 
-    const addTodo= (event)=>{
+    setTitle("");
+  };
+  const removeTodo = (userId)=>{
 
-        event.preventDefault()
+    setTodos(prev=>{
+        return prev.filter(todo =>todo.id!==userId)
+    })
 
-        if(title){
-
-
-            let newTodoObject={
-                id:todos.length+1,
-                todoTitle:title,
-                status:false
-            }
-            setTodos(prev=>{
-
-                return [...prev,newTodoObject]
-              })
-        }
-
-        
+    console.log(userId);
+}
 
 
-      
-
-        setTitle("")
-
-       
-
-        
-        
-    }
-    
-    console.log(todos);
-
-
+  console.log(todos);
 
   return (
-
     <div>
-        <Header/>
+      <Header />
 
-        <div>
-            <form onSubmit={addTodo} className="flex justify-center items-center gap-3 mt-5 " action="">
-                <div className="w-[40%] relative ">
-                <input onChange={todoTitleHandler} value={title} className="p-2 pr-10 w-full " placeholder="Enter todo..." type="text"name="todo-name" />
-                <button type="submit">
-                <Icon className=" hover:bg-blue-300 hover:text-white text-green-400  absolute right-0 top-1/2 transform -translate-y-1/2" icon="mynaui:plus-square" width="40" height="40" />
-                </button>
-                
+      <div>
+        <form
+          onSubmit={addTodo}
+          className="flex justify-center items-center gap-3 mt-5 "
+          action=""
+        >
+          <div className="w-[40%] relative ">
+            <input
+              onChange={todoTitleHandler}
+              value={title}
+              className="p-2 pr-10 w-full "
+              placeholder="Enter todo..."
+              type="text"
+              name="todo-name"
+            />
+            <button type="submit">
+              <Icon
+                className=" hover:bg-blue-300 hover:text-white text-green-400  absolute right-0 top-1/2 transform -translate-y-1/2"
+                icon="mynaui:plus-square"
+                width="40"
+                height="40"
+              />
+            </button>
+          </div>
+          <select className="p-2" name="todos" id="todo-option">
+            <option value="all">all</option>
+            <option className="text-green-600" value="completed">
+              completed
+            </option>
+            <option className="text-red-500" value="Uncompleted">
+              Uncompleted
+            </option>
+          </select>
+        </form>
+      </div>
 
-                </div>
-                <select className="p-2" name="todos" id="todo-option">
-                <option value="all">all</option>
-                <option className="text-green-600" value="completed">completed</option>
-                <option className="text-red-500" value="Uncompleted">Uncompleted</option>
-
-                </select>
-
-                
-
-            </form>
-
-        </div>
-
-        <div className="todo_container mt-10 flex flex-col justify-center items-center gap-3">
-
-            {todos.map(todo=>{
-
-               return <Todos key={todo.id} {...todo}/>
-
-
-            })}
-
-              
-        </div>
-
-
+      <div className="todo_container mt-10 flex flex-col justify-center items-center gap-3">
+        {todos.map((todo) => {
+          return <Todos key={todo.id} {...todo} onRemove={removeTodo} />;
+        })}
+      </div>
     </div>
-  )
+  );
 }
--- src/Components/Todos.jsx
+++ src/Components/Todos.jsx
@@ -1,27 +1,25 @@
 import { Icon } from "@iconify-icon/react/dist/iconify.js";
 
-export default function Todos({id,todoTitle,status}) {
-    console.log(id);
-    console.log(todoTitle);
-    console.log(status);
-
-  
-
-
-
-    
-
-
+export default function Todos({ id, todoTitle, status, onRemove }) {
+  console.log(id);
+  console.log(todoTitle);
+  console.log(status);
+  console.log(onRemove);
 
   return (
     <div className="w-[350px] pl-2 bg-purple-300 flex items-center justify-between rounded-lg ">
       <span>{todoTitle}</span>
       <div className="flex items-center">
-
-        
-        
-        <Icon className=" bg-green-400 cursor-pointer hover:bg-green-300" icon="mynaui:check-solid" width="35" height="35" />
         <Icon
+          className=" bg-green-400 cursor-pointer hover:bg-green-300"
+          icon="mynaui:check-solid"
+          width="35"
+          height="35"
+        />
+        <Icon
+          onClick={() => {
+            onRemove(id);
+          }}
           className="bg-red-400 cursor-pointer hover:bg-red-300 "
           icon="mynaui:trash"
           width="35"
